3f2e1d6906b5320e4f4fcf942a9eda3ad0e6c7ef
Eliezer Wohl <eliezerwohl@gmail.com>
2018-01-20 19:45:27 -0500

    aboutStart

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php /* Template Name: About */ ?>
+<?php get_header(); ?>
+<div class="parallax-window" data-parallax="scroll" data-image-src="<?php bloginfo('template_url'); ?>/img/upper.png" class="upper">
+  <div class="about-container">
+    <h1>About</h1>
+    <p>Certified Personal Trainer</p>
+    <p>Certified Kickboxing Instructor</p>
+    <p>Registered Yoga Teacher</p>
+  </div>
+  <div class="bio-container">
+    <div class="bio bio-top">
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    </div>
+  </div>
+</div>
+<div class="parallax-window" data-parallax="scroll" data-image-src="<?php bloginfo('template_url'); ?>/img/lower.png" class="lower">
+  <div class="bio-container">
+    <div class="bio bio-bottom">
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    </div>
+  </div>
+  <br><br><br><br><br><br><br><br><br>
+</div>
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
+<style type="text/css">
+</style>
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -36,8 +36,16 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-        <li><a href="#" data-toggle="modal" data-target="#myModal">Get a FREE CONSULTATION <i class="fa fa-envelope" aria-hidden="true"></i></a></li></ul>
-
+            <li>
+            <?php if (!(is_front_page())) { ?>
+              <a class="brand" href="#">
+                <img src="<?php bloginfo('template_url'); ?>/img/logo-yellow.png">
+              </a>
+            <?php } else { ?>
+              <a href="#" data-toggle="modal" data-target="#myModal">Get a FREE CONSULTATION <i class="fa fa-envelope" aria-hidden="true"></i></a>
+            <?php } ?>
+          </li>
+        </ul>
           <?php
               wp_nav_menu( array(
                   'menu'              => 'primary',
