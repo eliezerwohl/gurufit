5dc1da7ee3ddae00ff6ac08ecb83e0e17a55333a
Eliezer Wohl <eliezerwohl@gmail.com>
2018-01-23 15:32:47 -0500

    added acf to about page

--- about.php
+++ about.php
@@ -3,17 +3,15 @@
 <div class="page-background">
   <div class="about-container">
     <h1>About</h1>
-    <p>Certified Personal Trainer</p>
-    <p>Certified Kickboxing Instructor</p>
-    <p>Registered Yoga Teacher</p>
+    <?php while ( have_rows( 'about_section') ) { the_row(); ?>
+      <p><?php the_sub_field("txt"); ?></p>
+    <?php }; ?>
   </div>
   <div class="white-container">
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    <?php the_field("main"); ?>
+    <div class='text-center'>
+      <button class="btn-blue">Contact Me</button>
+    </div>
   </div>
   <br><br><br><br><br><br><br><br><br><br>
   <?php if( have_rows( 'image_gallery') ){ ?>
@@ -23,7 +21,7 @@
       <div class="col-md-12">
         <div class="carousel slide multi-item-carousel" id="theCarousel">
           <div class="carousel-inner">
-            <?php while ( have_rows( 'image_gallery') ) { the_row(); ?> 
+            <?php while ( have_rows( 'image_gallery') ) { the_row(); ?>
             <div class="item <?php if ($first){ echo 'active';  $first = false; } ?>">
               <?php $image=get_sub_field('img'); ?>
               <div class="col-xs-4"><a href="#1"><img class="img-pop img-responsive" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>"></a></div>
@@ -37,11 +35,11 @@
     </div>
   </div>
   <?php }; ?>
-  <div id="myModal" class="modal">
+<div id="myModal" class="modal super-modal">
     <!-- The Close Button -->
-    <span class="close">&times;</span>
+    <span class="close-modal">&times;</span>
     <!-- Modal Content (The Image) -->
-    <img class="modal-content" id="img01">
+    <img class="modal-content super-modal-content" id="img01">
     <!-- Modal Caption (Image Text) -->
     <!--   <div id="caption"></div> -->
   </div>
